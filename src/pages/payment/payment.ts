542bc0a4a9ab45a6736c9072860dfb60197d479d
Alan <alanlambari@gmail.com>
2018-10-23 16:24:59 -0300

    Tela de confirmação de pedido

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -9,21 +9,28 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   templateUrl: 'payment.html',
 })
 export class PaymentPage {
-   pedido: PedidoDTO;
-   parcelas: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-   formGroup: FormGroup;
-   constructor(
+
+  pedido: PedidoDTO;
+
+  parcelas: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  formGroup: FormGroup;
+
+  constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public formBuilder: FormBuilder) {
-     this.pedido = this.navParams.get('pedido');
-     this.formGroup = this.formBuilder.group({
+
+    this.pedido = this.navParams.get('pedido');
+
+    this.formGroup = this.formBuilder.group({
       numeroDeParcelas: [1, Validators.required],
       "@type": ["pagamentoComCartao", Validators.required]
     });
   }
-   nextPage() {
+
+  nextPage() {
     this.pedido.pagamento = this.formGroup.value;
-    console.log(this.pedido);
+    this.navCtrl.setRoot('OrderConfirmationPage', {pedido: this.pedido});
   }
 }
