1273c474d647b0e2cb295c9b80a2d8a25f0a9eca
Alan <alanlambari@gmail.com>
2018-10-23 13:03:48 -0300

    Acesso ao carrinho: botão flutuante e menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,6 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AuthService } from '../services/auth.service';
 
-
-
-
 @Component({
   templateUrl: 'app.html'
 })
@@ -18,17 +15,21 @@ export class MyApp {
   pages: Array<{title: string, component: string}>;
 
   constructor(
-      public platform: Platform, 
-      public statusBar: StatusBar, 
-      public splashScreen: SplashScreen,
-      public auth : AuthService) 
-    {
-        this.initializeApp();
-      this.pages = [
-        { title: 'Profile', component: 'ProfilePage' },
-        { title: 'Categorias', component: 'CategoriasPage' },
-        { title: 'Logout', component: '' }
-      ];
+    public platform: Platform, 
+    public statusBar: StatusBar, 
+    public splashScreen: SplashScreen,
+    public auth: AuthService
+  ) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Profile', component: 'ProfilePage' },
+      { title: 'Categorias', component: 'CategoriasPage' },
+      { title: 'Carrinho', component: 'CartPage'},
+      { title: 'Logout', component: ''}
+    ];
+
   }
 
   initializeApp() {
@@ -40,15 +41,16 @@ export class MyApp {
     });
   }
 
-  openPage(page : {title: string, component: string}) {
-      switch(page.title){
-        case 'Logout':
-        this.auth.logout();
-        this.nav.setRoot('HomePage');
-        break;
+  openPage(page : {title:string, component:string}) {
 
-        default:
-          this.nav.setRoot(page.component);
-      }
+    switch (page.title) {
+      case 'Logout':
+      this.auth.logout();
+      this.nav.setRoot('HomePage');
+      break;
+
+      default:
+      this.nav.setRoot(page.component);
+    }
   }
 }
